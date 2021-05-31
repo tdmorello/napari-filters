c2a3506d9e7d3267887210102a1b35bf9f8a5758
Tim Morello <tdmorello@gmail.com>
2021-05-31 13:32:15 -0400

    Test github workflow

--- napari_filters/_dock_widget.py
+++ napari_filters/_dock_widget.py
@@ -8,19 +8,47 @@ Replace code below according to your needs.
 """
 from time import sleep
 from typing import TYPE_CHECKING
-
+from magicgui import magic_factory
 from napari_plugin_engine import napari_hook_implementation
 from qtpy.QtWidgets import QLabel, QPushButton, QVBoxLayout, QWidget
 
 if TYPE_CHECKING:
     import napari
 
+# class ExampleQWidget(QWidget):
+#     # your QWidget.__init__ can optionally request the napari viewer instance
+#     # in one of two ways:
+#     # 1. use a parameter called `napari_viewer`, as done here
+#     # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
+#     def __init__(self, napari_viewer):
+#         super().__init__()
+#         self.viewer = napari_viewer
+
+#         btn = QPushButton("Click me!")
+#         btn.clicked.connect(self._on_click)
+
+#         self.setLayout(QHBoxLayout())
+#         self.layout().addWidget(btn)
+
+#     def _on_click(self):
+#         print("napari has", len(self.viewer.layers), "layers")
+
+
+# @magic_factory
+# def example_magic_widget(img_layer: "napari.layers.Image"):
+#     print(f"you have selected {img_layer}")
+
+
+# @napari_hook_implementation
+# def napari_experimental_provide_dock_widget():
+#     # you can return either a single widget, or a sequence of widgets
+#     return [ExampleQWidget, example_magic_widget]
 
 def widget_wrapper():
     """Wrap a QWidget to make compatible with napari threading."""
     from napari.qt.threading import thread_worker
 
-    class RealPythonExample(QWidget):
+    class ExampleQWidget(QWidget):
         def __init__(self, viewer: 'napari.viewer.Viewer') -> None:
             super().__init__()
             self.clicksCount = 0
