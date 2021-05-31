f944a4da13aa131b353131cfb892dc9641d573f6
Tim Morello <tdmorello@gmail.com>
2021-05-31 13:55:40 -0400

    Test github workflow

--- napari_filters/_tests/test_dock_widget.py
+++ napari_filters/_tests/test_dock_widget.py
@@ -4,7 +4,7 @@ import pytest
 # this is your plugin name declared in your napari.plugins entry point
 MY_PLUGIN_NAME = "napari-filters"
 # the name of your widget(s)
-MY_WIDGET_NAMES = ["Example Q Widget"]
+MY_WIDGET_NAMES = ["widget_wrapper"]
 
 
 @pytest.mark.parametrize("widget_name", MY_WIDGET_NAMES)
